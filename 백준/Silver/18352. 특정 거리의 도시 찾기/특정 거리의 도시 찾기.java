bbdd6a38fe38f1af9d82de243f8110a07df87606
LeeGyeongYoon-BE <jack3614@naver.com>
2025-11-11 17:28:07 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1072 ms, Memory: 277504 KB -BaekjoonHub

---
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static ArrayList<Integer>[] graph;
+    static int N, M, K, X;
+    static int[] dist;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        } // for
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int S = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
+            graph[S].add(E); // 단방향
+        } // for
+
+        dist = new int[N + 1];
+        Arrays.fill(dist, -1); // -1 = 방문 안 함
+        bfs(X);
+
+        boolean found = false;
+        for (int i = 1; i <= N; i++) {
+            if (dist[i] == K) {
+                System.out.println(i);
+                found = true;
+            } // if
+        } // for
+
+        if (!found) System.out.println(-1);
+    } // main
+
+    static void bfs(int start) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        dist[start] = 0;
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            for (int next : graph[cur]) {
+                if (dist[next] == -1) {
+                    dist[next] = dist[cur] + 1;
+                    q.add(next);
+                } // if
+            } // for
+        } // while
+    } // bfs
+} // class
