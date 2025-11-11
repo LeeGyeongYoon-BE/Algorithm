8e6f1c4ea9ecac2a384bee5278c8e22b1526d9e0
LeeGyeongYoon-BE <jack3614@naver.com>
2025-11-11 13:30:40 +0900

    Update combination.java

--- concept/combination.java
+++ concept/combination.java
@@ -1,1 +1,9 @@
-
+public static long nCr(int n, int r) {
+    if (r > n - r) r = n - r; // C(n, r) = C(n, n-r)
+    long result = 1;
+    for (int i = 0; i < r; i++) {
+        result *= (n - i);
+        result /= (i + 1);
+    } // for
+    return result;
+} // nCr
