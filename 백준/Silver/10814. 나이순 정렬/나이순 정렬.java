860d19c574768607e60ede043bfa9d6cc72784be
LeeGyeongYoon-BE <jack3614@naver.com>
2025-11-13 10:40:21 +0900

    [Silver V] Title: 나이순 정렬, Time: 1332 ms, Memory: 61500 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[][] members = new String[n][2];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            members[i][0] = st.nextToken();
+            members[i][1] = st.nextToken();
+        }
+
+        Arrays.sort(members, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] o1, String[] o2) {
+                return Integer.parseInt(o1[0]) - Integer.parseInt(o2[0]);
+            }
+        });
+
+        for (int i = 0; i < n; i++)
+            System.out.println(members[i][0] + " " + members[i][1]);
+    }
+}
