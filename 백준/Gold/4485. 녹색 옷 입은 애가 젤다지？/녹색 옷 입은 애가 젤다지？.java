df6fe58eb4820b9348fb997c51dbf0d43b86ef4a
LeeGyeongYoon-BE <jack3614@naver.com>
2025-12-19 16:10:43 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 744 ms, Memory: 25276 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -4,14 +4,12 @@ import java.util.*;
 public class Main {
     static int[][] map, distance;
     static boolean[][] visited;
-    static int[] pi = {-1, 1, 0, 0}; // plusi, plusj
-    static int[] pj = {0, 0, -1, 1}; // pi와 pj가 합쳐져서 새로운 상대 좌표를 표현
-
+    static int[] pi = {-1, 1, 0, 0};
+    static int[] pj = {0, 0, -1, 1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int testNo = 0;
-        while ( ++testNo > 0 ){
+        int num = 1;
+        while(true) {
             int N = Integer.parseInt(br.readLine());
             if (N == 0) break;
             map = new int[N][N];
@@ -27,41 +25,41 @@ public class Main {
             } // for - i
 
             distance[0][0] = map[0][0];
-            for(int k = 0; k < N*N; k++){ // for - k
+
+            for (int k = 0; k < N * N; k++) {
                 int min = Integer.MAX_VALUE;
                 int minI = 0;
                 int minJ = 0;
 
-                for (int i = 0; i < N; i++){
-                    for(int j = 0; j < N; j++){
-                        if ( !visited[i][j] && min > distance[i][j]){
+                for (int i = 0; i < N; i++) {
+                    for (int j = 0; j < N; j++) {
+                        if (!visited[i][j] && min > distance[i][j]) {
                             min = distance[i][j];
                             minI = i;
                             minJ = j;
-                        } // if
+                        }
                     } // for - j
                 } // for - i
 
                 visited[minI][minJ] = true;
-                if ( minI == N-1 && minJ == N-1){
-                    break; // 모든 순회를 끝마침
-                } // if
+                if( minI == N-1 && minJ == N-1 ) break; // 모든 순회를 끝마침
 
-                for (int d = 0; d < pi.length; d++){
+                for (int d = 0; d < pi.length; d++) {
                     int ni = minI + pi[d];
                     int nj = minJ + pj[d];
 
-                    if ( ni < 0 || ni >= N || nj < 0 || nj >= N) continue;
+                    if (ni < 0 || ni >= N || nj < 0 || nj >= N) continue;
 
-                    if ( distance[ni][nj] > min + map[ni][nj] ) {
+                    if (distance[ni][nj] > min + map[ni][nj]) {
                         distance[ni][nj] = min + map[ni][nj];
                     } // if
-
                 } // for - d
             } // for - k
-            System.out.println("Problem " + testNo + ": " + distance[N-1][N-1]);
-        }
+
+            System.out.println("Problem " + num + ": " + distance[N-1][N-1]);
+            num++;
+        } // while
+
         br.close();
     } // main
-
 } // class
